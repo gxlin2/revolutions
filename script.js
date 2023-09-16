4067d9d6ccb987d35f6f3390c50bde07b070bfce
Alexa L <alexa.lowe@gmail.com>
2023-09-16 02:33:52 -0400

    version 2am

---
+++ script.js
@@ -0,0 +1,12 @@
+const minute = 1000 * 60;
+const hour = minute * 60;
+const day = hour * 24;
+const year = day * 365;
+
+// Divide Time with a year
+const d = new Date();
+let years = Math.round(d.getTime() / year);
+
+document.getElementById('test').innerHTML = years;
+
+
