a1189e174b7a91cad6652ec9bbfcabc38166cfc9
Alexa L <alexa.lowe@gmail.com>
2023-09-16 04:18:47 -0400

    rocket and lines

--- script.js
+++ script.js
@@ -1,11 +1,17 @@
+// Divide time with these
 const minute = 1000 * 60;
 const hour = minute * 60;
 const day = hour * 24;
 const year = day * 365;
 
-// Divide Time with a year
+// Days on Earth
 const d = new Date();
-let years = Math.round(d.getTime() / year);
+let days = Math.floor(d.getTime() / day);
+document.getElementById("days").innerHTML = "Day " + days + " on Earth.";
 
-document.getElementById("test").innerHTML = years;
+// Years on Earth
+let years = Math.floor(d.getTime() / year);
+let percent_complete = Math.round((days % 365) * 100 / 365);
+document.getElementById("years").innerHTML = "Revolution " + years + " around the sun is " + percent_complete + "% complete.";
+
 
